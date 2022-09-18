7c9bbac8980ba4053c9db7566e1d352db67bf5a8
Taylor Perkins <taylor.perkins@alt-tab-design.com>
2022-09-18 17:17:51 -0500

    define the rekognition config

--- config/rekognition.php
+++ config/rekognition.php
@@ -2,5 +2,7 @@
 
 // config for LaravelGeek/LaravelRekognition
 return [
-
+    'key'    => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
 ];
