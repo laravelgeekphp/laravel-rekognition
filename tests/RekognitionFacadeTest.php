24c073f82911ad932f103c97baabb6046355e21d
Taylor Perkins <taylor.perkins@alt-tab-design.com>
2022-09-25 14:17:22 -0500

    update test to expect InvalidArgumentException

--- tests/RekognitionFacadeTest.php
+++ tests/RekognitionFacadeTest.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Storage;
 use LaravelGeek\LaravelRekognition\Facades\Rekognition;
 
 it('returns null given bad data', function () {
-    expect(Rekognition::getFromFilePath('badFilePath.jpg'))->toBeNull();
-});
+    Rekognition::getFromFilePath('badFilePath.jpg');
+})->throws(InvalidArgumentException::class);
 
 it('returns null if file has no contents', function () {
     Storage::fake();
